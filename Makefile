2dfdba24f4d962ddb69441d355d47602cd0dfbaa
Kalvis Kalniņš <kalvis@factury.co>
2020-02-23 22:10:14 +0200

    chore: use readme for PyPI description

--- Makefile
+++ Makefile
@@ -4,8 +4,13 @@ init:
 test:
 	pytest
 
-upload:
+clean:
+	rm -rf build dist beancount_oneliner.egg-info/
+
+build: clean
 	python3 setup.py sdist bdist_wheel
+
+upload: build
 	twine upload dist/*
 
 .PHONY: init test
--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 
 from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='beancount_interpolate',
     version='2.1.3',
     description='Plugins for Beancount to interpolate transactions',
-    long_description=
-    """
-Four plugins for double-entry accounting system Beancount to interpolate transactions by generating additional entries over time.
 
-- `recur`: dublicates all entry postings over time
-- `split`: dublicates all entry postings over time at fraction of value
-- `depr`: generates new entries to depreciate target asset/liability posting over given period
-- `spread`: generate new entries to allocate P&L of target income/expense posting over given period
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
-These plugins are triggered by adding metadata or tags to source entries. It's safe to disable at any time. All plugins share the same parser that can set maximal period, custom starting date and minimal step by either number or keyword.
-
-You can use these to define recurring transactions, account for depreciation, smooth transactions over time and make graphs less zig-zag.
-
-    """,
-    license='GNU GPLv3',
     author='Kalvis \'Akuukis\' Kalnins',
     author_email='akuukis@kalvis.lv',
-    url='https://github.com/Akuukis/beancount_interpolate',
     download_url='https://pypi.python.org/pypi/beancount_interpolate',
+    license='GNU GPLv3',
+    package_data={'beancount_interpolate': ['../README.md']},
     package_dir={'beancount_interpolate': 'beancount_interpolate'},
     packages=['beancount_interpolate'],
-    package_data={'beancount_interpolate': ['../README.md']},
     requires=['beancount (>2.0)'],
-     )
+    url='https://github.com/Akuukis/beancount_interpolate',
+)
