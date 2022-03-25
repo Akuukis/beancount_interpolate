7d8622876ac1ce764e99c238f75aa3d8c14bfbe6
Akuukis <akuukis@kalvis.lv>
2022-03-25 11:56:09 +0200

    chore: publish 2.5.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='beancount_interpolate',
-    version='2.4.1',
+    version='2.5.0',
     description='Plugins for Beancount to interpolate transactions',
 
     long_description=long_description,
